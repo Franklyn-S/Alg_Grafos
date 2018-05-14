ce2565a7ef699a8df7c288de4426f9d931f963c5
Franklyn Seabra <franklynseabra@alu.ufc.br>
2018-05-14 14:01:27 -0300

    Falta terminargit add .!

--- CompConexas.py
+++ CompConexas.py
@@ -1,61 +1,70 @@
 #_*_ coding: utf8 _*_
-class No(object):
-	def __init__(self, id):
-		self.id = int(id)
-		self.vizinhos = []
-		self.atingido = False
+# class No(object):
+# 	def __init__(self, id):
+# 		self.id = int(id)
+# 		self.vizinhos = []
+# 		self.atingido = False
 
-	def addVizinho(self, no):
-		self.vizinhos.append(no)
-		no.vizinhos.append(self)
+# 	def addVizinho(self, no):
+# 		self.vizinhos.append(no)
+# 		no.vizinhos.append(self)
 
-	def __str__(self):
-		return str(self.id)
+# 	def __str__(self):
+# 		return str(self.id)
 
-def BFS(*grafo, No):
-	componente = []
-	fila = []
-	fila.append(No)
-	while not fila:
-		u = fila.pop()
-		u.atingido = True
-		componente.append(u)
-		for v in u.vizinhos:
-			if not v.atingido:
-				fila.append(v)
-				v.atingido = True
-	return componente.sort(key = id)
+# def BFS(*grafo, No):
+# 	componente = []
+# 	fila = []
+# 	fila.append(No)
+# 	while not fila:
+# 		u = fila.pop()
+# 		u.atingido = True
+# 		componente.append(u)
+# 		for v in u.vizinhos:
+# 			if not v.atingido:
+# 				fila.append(v)
+# 				v.atingido = True
+# 	return componente.sort(key = id)
 
-def main():
+#def main():
 
-	#input("")
-	#input("")
-	s = input("")
-	#input("")
+dl = input("")
+format = input("")
 
-	n1, n = s.split("=")
-	n = int(n)
+num_vertice = int(input().split("=")[1])
 
-	grafo = []
-	for i in range(1,n+1):
-		grafo.append(No(i))
+data = input()
 
-	aux = input("")
-	while aux != "":
-		v1, v2 = aux.split(" ")
-		v1 = int(v1)
-		v2 = int(v2)
-		grafo[v1 - 1].vizinhos.addVizinho(v2)
-		aux = input("")
+aux = input().split()
 
-	for No in grafo:
-		if not No.atingido:
-			s = ""
-			for i in BFS(*grafo, NO):
-				print(i, end = " ")
-			print("\n")
+grafo = []
+atingidos = []
+LA = []
+for x in xrange(1,num_vertice+1):
+	atingidos.append(False)
+	LA.append([])
+
+int i = 0
+while aux != []:
+	v1 = int(aux[0])
+	v2 = int(aux[1])
+	aresta = [v1, v2]
+	grafo[i].append(aresta)
+	i++
+	try:
+		print(aux)
+		aux = input().split()
+	except EOFError:
+		break
+
+for n in grafo:
+	if not n.atingido:
+		s = ""
+		for i in BFS(*grafo, n):
+			print(i, end = " ")
+		print("\n")
 
 
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# main()
 
