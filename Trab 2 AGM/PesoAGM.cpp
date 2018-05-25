683983059926058d47f2f21d40717fb621f98f43
Franklyn Seabra Rogério Bezerra <franklynseabra@alu.ufc.br>
2018-05-25 13:58:34 -0300

    Entrada de dados

--- Trab 2 AGM/PesoAGM.cpp
+++ Trab 2 AGM/PesoAGM.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
-
+#include <cstdio>
+#include <cstring>
+#include <list>
 using namespace std;
 
-int main(void)
-{
-string dl;
-cin >> dl;
-cout << dl;
-cout << endl;
+class Grafo{
+	int V;//número de vertices
+	list<int> *adj; //ponteiro para um array contendo as listas de adjacencia
+};
+
+
+int main(void){
+	//Declarações
+	string dl, aux, format, data;
+	int n;
+
+	int v1, v2;
+	float p;
+
+	//Entrada de dados
+	//cin >> dl;
+	//cin >> format;
+	//cin >> aux;
+	
+	//n = std::strtok(aux, "=");
+
+	//isolando a quantidade de vertices n
+	while(!scanf("n=%d", &n)){getchar();}
+	n = int(n);
+
+	//tratando entrada
+	while(getchar() != ':'){}
+	getchar();
+
+	//receber o grafo
+	int contador = 0;
+	while(contador < n){
+		//recebe a aresta (v1, v2) com peso p
+		cin >> v1 >> v2 >> p;
+		//armazenar no grafo
+		
+		contador++;
+	}
+	
+	
+
+	
+	
 }
