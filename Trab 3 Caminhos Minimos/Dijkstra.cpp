f30310d06c399db5469477dbdc12e68fa0dcf72a
Franklyn Seabra <franklynseabra12@hotmail.com>
2018-06-24 19:11:52 -0300

    Consertar os erros dessa bagaça

--- Trab 3 Caminhos Minimos/Dijkstra.cpp
+++ Trab 3 Caminhos Minimos/Dijkstra.cpp
@@ -9,22 +9,34 @@
 
 using namespace std;
 
+
 class No
 {
-	int vertice1, chave;
+	int vertice;
+	double chave;
 	bool atingido;
 public:
 
-	No(int v1)
+	No(int v)
 	{
-		vertice1 = v1;
-		chave = -1;//distancia
+		vertice = v;
+		chave = std::numeric_limits<double>::max();//distancia
 		atingido = false;
 	}
 
-	int obterVertice1()
+	void atingir()
 	{
-		return vertice1;
+		this->atingido = true;
+	}
+
+	void setChave(double chave)
+	{
+		this->chave = chave;
+	}
+
+	int obterVertice()
+	{
+		return vertice;
 	}
 
 	int obterChave()
@@ -39,37 +51,196 @@ public:
 	}
 };
 
+class Aresta
+{
+	No no1, no2;
+	double peso;
+public:
+
+	Aresta(No no1, No no2, double peso)
+	{
+		this->no1 = no1;
+		this->no2 = no2;
+		this->peso = peso;
+	}
+
+	double obterPeso()
+	{
+		return peso;
+	}
+};
+
+
+class heap
+{
+	std::vector<No> H;
+	double ultimo;
+public:
+	heap()
+	{	
+		H.push_back(0);
+		ultimo = 0;
+	}
+
+	void pai(int i){
+		return (i >> 1);
+	}
+
+	void esquerda(int i)
+	{
+		return (i << 1);
+	}
+
+	void direita(int i)
+	{
+		return (i << 1) + 1;
+	}
+
+	void descer(int i)
+	{
+		int e = i.esquerda();
+		int d = i.direita();
+		int menor;
+		if(e <= this->ultimo and this->H[e].obterChave() < this->H[i].obterChave()){
+			menor = e;
+		}else{
+			menor = i;
+		}
+		if(d <= this->ultimo and this->H[d].obterChave() < this->H[menor].obterChave()){
+			menor = d;
+		}
+
+		if (menor != i){
+			No aux = this->H[i];
+			this->H[i] = this->H[menor];
+			this->H[menor] = aux;
+			this->descer(menor);
+		}
+	}
+
+	No removerMinimo()
+	{
+		if (this->ultimo == 0){
+			return NULL;
+		}
+		No minimo = this->H[1];
+		this->H[1] = this->H[this->ultimo];
+		this->ultimo -= 1;
+		if(this->ultimo >= 1){
+			this->descer(1);
+		}
+		this->H.pop_back(this->ultimo + 1);
+		return minimo;
+	}
+
+	void incluir(No v, double k){
+		this->ultimo ++;
+		this->H.push_back(v);
+		this->aumentarPrioridade(v, k);
+	}
+
+	void aumentarPrioridade(No u, double k){
+		int i = 0;
+		while( i < this->H.size() and this->H[i] != u) 
+		{
+			i++;
+		}
+		if !(k > this->H[i].obterChave()){
+			this->H[i].setChave(k);
+
+			while(i >= 2 and H[pai(i)].obterChave() > H[i].obterChave()){
+				No aux = H[i];
+				H[i] = H[pai(i)];
+				H[pai(i)] = aux;
+				i = pai(i);
+			}
+		}
+
+	}
+	double pegaPeso(int vertice){
+		for (int i = 0; i < H.size(); i++)
+		{
+			if(H[i].vertice == vertice){
+				return H[i].obterChave();
+			}
+		}
+		return 0;
+	}
+};
 
 
 class Grafo
 {
 	int V;//numero de vértices
 	vector<Aresta> arestas; //vetor de arestas
+	list<No> *adj;
+	vector<bool> pertenceHeap;//vetor auxiliar para verificar se pertence à Heap
 
 
 public:
 	Grafo(int V)
 	{
 		this->V = V;
+		adj = new list<No>[V];
+		for (int i = 0; i < V; i++)
+		{
+			pertenceHeap.push_back(false);
+		}
+	}	
+
+	double obterPeso(int v1, int v2)
+	{
+		for (int i = 0; i < arestas.size(); i++)
+		{
+			if(arestas[i].no1.vertice == v1 and arestas[i].no2.vertice == n2){
+				return arestas[i].obterPeso();
+			}
+		}
 	}
 
 	void adicionarAresta(int v1, int v2, double peso)
 	{	
-		adj[v1].push_back(v2);
-		Aresta aresta(v1, v2, peso);
+		No n1 = No(v1);
+		No n2 = No(v2)
+		Aresta aresta(n1, n2, peso);
 		arestas.push_back(aresta);
+		adj[v1].push_back(n2);
 	}
 
 	//Implementar as funções do Dijkstra aqui
-	void dijkstra()
+	void dijkstra(vector<No> vertices)
 	{
-		distancia[0] = 0;
-		for(int i = 0; i < V; i++)
-		{
-			distancia.push_back(std::numeric_limits<double>::max());	
-			atingido.push_back(false);
+		vertices[0].atingir();
+		vertices[0].setChave(0.000);
+		heap hp = heap();
+		//percorrer vizinhos
+		No no; //auxiliar
+		for(int i = 0; i < adj[0].size(); i++){
+			
+			adj[0][i].atingir();
+			adj[0][i].
+			hp.incluir(adj[0][i], obterPeso(0, adj[0][i].obterVertice()));
+			pertenceHeap[adj[0][i].obterVertice()] = true;
 		}
-		
+		while(hp != [0]){
+			no = hp.removerMinimo();
+			vertices[no.vertice] = no.obterChave(); //distancia[u] = u.chave
+			for (int i = 0; i < this->adj[no.obterVertice()].size(); i++) //Para cada v E N(u)
+			{
+				if(!adj[no.obterVertice()][i].atingido){ //Se não atingido
+					adj[no.obterVertice()][i].atingir();
+					hp.incluir(adj[no.obterVertice()][i], obterPeso(no.obterVertice(), adj[no.obterVertice()][i].obterVertice()));
+					pertenceHeap[adj[no.obterVertice()][i].obterVertice()] = true;
+				}else{
+
+					double pesoOutroCaminho = vertices[no.obterVertice()] + obterPeso(no.obterVertice(), adj[no.obterVertice()][i].obterVertice());
+					if (pertenceHeap[adj[no.obterVertice()][i].obterVertice()] and 
+						pesoOutroCaminho < hp.pegaPeso(adj[no.obterVertice()][i].obterVertice())){
+							hp.aumentarPrioridade(adj[no.obterVertice()][i], pesoOutroCaminho);
+					}
+				}
+			}
+		}
 
 
 	}
@@ -86,6 +257,7 @@ int main(int argc, char *argv[]){
 
 	while(!scanf("n=%d", &n)){ getchar();}
 	n = int(n);
+
 	std::vector<No> vertices;
 	for (int i = 0; i < n; i++)
 	{
@@ -105,5 +277,17 @@ int main(int argc, char *argv[]){
 		g.adicionarAresta(v1-1, v2-1, p);
 	}
 
-	g.dijkstra();
+	g.dijkstra(vertices);
+
+
+	for (int i = 0; i < vertices.size(); i++)
+	{
+		if(vertices[i].obterChave() == std::numeric_limits<double>::max()){
+			cout << i << " INFINITO" << endl;
+		}else{
+		std::cout.precision(3);
+		cout << i << " ";
+		std::cout << std::fixed << vertices[i].obterChave() << endl;
+		}
+	}
 }
